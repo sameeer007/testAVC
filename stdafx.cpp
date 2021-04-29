ef00bd22bc3d50d45991eba918e9448af34eaf0a
Your Name <you@example.com>
2021-04-29 11:52:10 +0530

    clock added

--- stdafx.cpp
+++ stdafx.cpp
@@ -1,6 +1,6 @@
 
 // stdafx.cpp : source file that includes just the standard includes
-// TestApplication.pch will be the pre-compiled header
+// Test
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
